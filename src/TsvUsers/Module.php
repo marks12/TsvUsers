1e31a212731abcfb19a53dc96522320eb53c481b
tsarevnikov <tsarevnikov@srv-web-02.entc.nsk>
2014-06-09 15:55:18 +0700

    Error path was fixed

--- src/TsvUsers/Module.php
+++ src/TsvUsers/Module.php
@@ -19,7 +19,7 @@ class Module implements AutoloaderProviderInterface
     {
         return array(
             'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
+                __DIR__ . '/../../autoload_classmap.php',
             ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
@@ -32,7 +32,7 @@ class Module implements AutoloaderProviderInterface
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
     public function onBootstrap(MvcEvent $e)
