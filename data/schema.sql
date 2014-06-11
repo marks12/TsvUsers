1b95277078c5af3adbc6d8dc824898232e8bc39b
tsarevnikov <tsarevnikov@srv-web-02.entc.nsk>
2014-06-11 11:10:55 +0700

    Config file and SQL dump added

---
+++ data/schema.sql
@@ -0,0 +1,65 @@
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `display_name` varchar(50) DEFAULT NULL,
+  `password` varchar(128) NOT NULL,
+  `state` smallint(5) unsigned DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `email`, `display_name`, `password`, `state`) VALUES
+(3, NULL, 'tsarevnikov@mail.ru', NULL, '$2y$14$itlOcTYgE8HwBJbU1/iGH.mWtossQ/X/lQIuVVKBTHsRhwkJMF5wC', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roleId` varchar(255) NOT NULL,
+  `is_default` tinyint(1) NOT NULL,
+  `parent_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `roleId`, `is_default`, `parent_id`) VALUES
+(2, 'user', 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role_linker`
+--
+
+CREATE TABLE IF NOT EXISTS `user_role_linker` (
+  `user_id` int(11) unsigned NOT NULL,
+  `role_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `role_id` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_role_linker`
+--
+
+INSERT INTO `user_role_linker` (`user_id`, `role_id`) VALUES
+(3, 'admin'),
+(3, 'user');
